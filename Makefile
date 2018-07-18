54505e62ad5c474cf4573be7861411b3056b0bd3
leios <jrs.schloss@gmail.com>
2018-07-19 07:06:57 +0900

    adding scenes for split_op animation.

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@
 
 CXX = g++
 #CXXFLAGS = -std=c++14 -g -Wall -march=native -fopenmp -fno-omit-frame-pointer -O2 -flto -Wno-sign-compare -fsanitize=address
-CXXFLAGS = -std=c++14 -g -O2 #-Wall -march=native -fopenmp -fno-omit-frame-pointer -flto -Wno-sign-compare
+CXXFLAGS = -std=c++14 -g -O2 -lfftw3 #-Wall -march=native -fopenmp -fno-omit-frame-pointer -flto -Wno-sign-compare -lfftw
 
 LIBFLAGS = `pkg-config --cflags --libs cairo libavformat libavcodec libswresample libswscale libavutil`
 BINS = vis_test
--- src/vis_test.cpp
+++ src/vis_test.cpp
@@ -2,78 +2,184 @@
 #include <cmath>
 #include <memory>
 #include <vector>
+#include <fftw3.h>
 
 #include "../include/camera.h"
 #include "../include/scene.h"
 
 void first_scene(camera& cam, scene& world) {
-    auto title = std::make_shared<text>(vec{0, 100}, 50, "GathVL Test");
+    int xrange = 1200;
+    int yrange = 300;
+    int res = 500;
 
-    auto ball = std::make_shared<ellipse>(vec{640, 360}, vec{0, 0}, 0, true);
+    auto y_axis = std::make_shared<line>(vec{50, cam.size.y-50},
+                                         vec{50, cam.size.y-50});
+    auto x_axis = std::make_shared<line>(vec{50, cam.size.y-50},
+                                         vec{50, cam.size.y-50});
 
-    ball->add_animator<vec_animator>(0, 49, &ball->size, vec{0, 0},
-                                     vec{50,50});
-
-    ball->add_animator<vec_animator>(50, 99, &ball->location, vec{640, 360},
-                                     vec{-50, -50});
-
-    auto rec = std::make_shared<rectangle>(vec{-50, -50}, vec{50, 50}, M_PI / 4,
-                                           true);
-
-    rec->add_animator<vec_animator>(100, 149, &rec->location, vec{-50, -50},
-                                    vec{640, 360});
-
-    rec->add_animator<vec_animator>(150, 199, &rec->size, vec{50, 50},
-                                    vec{0, 0});
-
-    world.add_layer();
-    world.add_shape(title, 0);
-    world.add_shape(ball, 1);
-    world.add_shape(rec, 1);
-
-    for (int i = 0; i < 200; ++i) {
-        world.update(i);
-        cam.encode_frame(world);
-    }
-
-    world.clear();
-}
-
-void second_scene(camera& cam, scene& world) {
-    auto title = std::make_shared<text>(vec{0, 720}, 50, "GathVL Test");
-
-    auto y_axis = std::make_shared<line>(vec{50, 10}, vec{50, 610});
-    auto x_axis = std::make_shared<line>(vec{1250, 610}, vec{50, 610});
-
+    int center_loc = xrange/2 + 50;
+    auto arrow_1 = std::make_shared<arrow>(vec{center_loc, 250}, 100, M_PI / 2);
+    auto arrow_2 = std::make_shared<arrow>(vec{center_loc, 250}, 100, M_PI / 2);
     std::vector<vec> exp_points;
 
-    vec origin = {50, 610};
+    vec origin = {50, cam.size.y-50};
 
-    for (int i = 0; i < 100; ++i) {
-        exp_points.emplace_back(i * 12, std::exp(i * 0.06) / 0.68);
+    for (int i = 0; i < res; ++i) {
+        double xpos = i/(double)res*20 - 10;
+        exp_points.emplace_back(i * xrange/res,
+                                std::exp(-(xpos*xpos)/5.0)*yrange);
     }
 
     auto exp_curve =
         std::make_shared<curve>(std::vector<vec>(), origin);
 
+    x_axis->add_animator<vec_animator>(0, 50, &x_axis->end, x_axis->start,
+                                       x_axis->start + vec(xrange, 0));
+    y_axis->add_animator<vec_animator>(0, 50, &y_axis->end, y_axis->start,
+                                       y_axis->start + vec(0, -yrange));
+
     exp_curve->add_animator<vector_animator<vec>>(0, 50, 0, exp_points,
                                                   &exp_curve->points);
+    arrow_1->add_animator<vec_animator>(50,101, &arrow_1->location,
+                                        vec{center_loc, 250},
+                                        vec{center_loc-xrange/4, 250});
+    arrow_2->add_animator<vec_animator>(50,101, &arrow_2->location,
+                                        vec{center_loc, 250},
+                                        vec{center_loc+xrange/4, 250});
 
-    world.add_shape(title, 0);
+    arrow_1->add_animator<vec_animator>(100,151, &arrow_1->location,
+                                        vec{center_loc-xrange/4, 250},
+                                        vec{center_loc, 250});
+    arrow_2->add_animator<vec_animator>(100,151, &arrow_2->location,
+                                        vec{center_loc+xrange/4, 250},
+                                        vec{center_loc, 250});
+
     world.add_shape(y_axis, 0);
     world.add_shape(x_axis, 0);
     world.add_shape(exp_curve, 0);
 
-    for (int i = 0; i < 51; ++i) {
+    for (int i = 0; i < 151; ++i) {
+        if (i == 50){
+            world.add_shape(arrow_1, 0);
+            world.add_shape(arrow_2, 0);
+        }
+        if (i >= 51 && i < 101){
+            double scale = (101-i)/50.0;
+            for (int j = 0; j < exp_curve->points.size(); ++j){
+                double xpos = j/(double)res*20 - 10;
+                exp_curve->points[j] = {j * xrange/res,
+                    scale*std::exp(-(xpos*xpos)/5.0)*yrange
+                    + (1 - scale)*pow(sin(2*M_PI*xpos/20),2)*yrange};
+
+            }
+        }
+        if (i >= 101 && i < 151){
+            for (int j = 0; j < exp_curve->points.size(); ++j){
+                double xpos = j/(double)res*20 - 10;
+                double scale = (151-i)/50.0;
+                exp_curve->points[j] = {j * xrange/res,
+                    (1 - scale)*std::exp(-(xpos*xpos)/5.0)*yrange
+                    + scale*pow(sin(2*M_PI*xpos/20),2)*yrange};
+
+            }
+
+        }
+            world.update(i);
+            cam.encode_frame(world);
+    }
+}
+
+void second_scene(camera& cam, scene& world) {
+    world.clear();
+
+    // Creating axis for plotting
+    int xrange = 500;
+    int yrange = 500;
+    int res = 200;
+
+    auto y_axis1 = std::make_shared<line>(vec{50, cam.size.y-50},
+                                          vec{50, cam.size.y-50});
+    auto x_axis1 = std::make_shared<line>(vec{50, cam.size.y-50},
+                                          vec{50, cam.size.y-50});
+
+    auto y_axis2 = std::make_shared<line>(vec{690, cam.size.y-50},
+                                          vec{690, cam.size.y-50});
+    auto x_axis2 = std::make_shared<line>(vec{690, cam.size.y-50},
+                                          vec{690, cam.size.y-50});
+
+    x_axis1->add_animator<vec_animator>(0, 50, &x_axis1->end, x_axis1->start,
+                                        x_axis1->start + vec(xrange, 0));
+    y_axis1->add_animator<vec_animator>(0, 50, &y_axis1->end, y_axis1->start,
+                                        y_axis1->start + vec(0, -yrange));
+
+    x_axis2->add_animator<vec_animator>(0, 50, &x_axis2->end, x_axis2->start,
+                                        x_axis2->start + vec(xrange, 0));
+    y_axis2->add_animator<vec_animator>(0, 50, &y_axis2->end, y_axis2->start,
+                                        y_axis2->start + vec(0, -yrange));
+
+    // FFT'ing gaussian curve for momentum-space image
+    std::vector<fftw_complex> gaussian(res), fft_out(res);
+
+    for (size_t i = 0; i < gaussian.size(); ++i){
+        double xpos = -5 + 10.0*(i / (double)gaussian.size());
+        gaussian[i][0] = std::exp(-(xpos*xpos)/.005);
+        gaussian[i][1] = 0;
+    }
+
+    // Creating FFTW plans and such
+    fftw_plan plan = fftw_plan_dft_1d(res, gaussian.data(), fft_out.data(),
+                                      FFTW_FORWARD, FFTW_ESTIMATE);
+
+    fftw_execute(plan);
+
+    // Finding renormalization factor
+    double renorm_factor = 0;
+    for (int i = 0; i < res; ++i){
+        renorm_factor += (fft_out[i][0]*fft_out[i][0]
+                         +fft_out[i][1]*fft_out[i][1]);
+    }
+    // Adding two gaussian curves
+    std::vector<vec> gaussian_points, fft_out_points;
+    vec origin1 = {50, cam.size.y-50};
+    vec origin2 = {690, cam.size.y-50};
+
+    auto gaussian_curve =
+        std::make_shared<curve>(std::vector<vec>(), origin1);
+    auto fft_out_curve =
+        std::make_shared<curve>(std::vector<vec>(), origin2);
+
+    for (int i = 0; i < res; ++i) {
+        double fft_yval = (fft_out[i][0]*fft_out[i][0]
+                           +fft_out[i][1]*fft_out[i][1]) / renorm_factor;
+        fft_out_points.emplace_back(i * xrange/res, fft_yval * yrange);
+        gaussian_points.emplace_back(i * xrange/res, gaussian[i][0]*yrange);
+    }
+
+    
+    gaussian_curve->add_animator<vector_animator<vec>>(0,50,0,gaussian_points,
+                                                       &gaussian_curve->points);
+
+    fft_out_curve->add_animator<vector_animator<vec>>(0,50,0,fft_out_points,
+                                                       &fft_out_curve->points);
+    world.add_shape(x_axis1, 0);
+    world.add_shape(y_axis1, 0);
+    world.add_shape(x_axis2, 0);
+    world.add_shape(y_axis2, 0);
+    world.add_shape(gaussian_curve, 0);
+    world.add_shape(fft_out_curve, 0);
+
+    for (int i = 0; i < 151; ++i) {
         world.update(i);
         cam.encode_frame(world);
     }
+
 }
 
 int main() {
     camera cam(vec{1280, 720});
     scene world = scene({1280, 720}, {0, 0, 0, 1});
 
+    //cam.add_encoder<png_encoder>();
     cam.add_encoder<video_encoder>("/tmp/video.mp4", cam.size, 50);
 
     first_scene(cam, world);
